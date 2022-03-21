bcafef5f284c060d16b7062688ca5021cd25fdfb
eliserust <eliserust@gmail.com>
2022-03-21 19:20:10 -0400

    reformatted OECD_students_munging.py

--- Munging_Scripts/OECD_students_munging.py
+++ Munging_Scripts/OECD_students_munging.py
@@ -28,48 +28,75 @@ for col in students.columns:
 students.describe()
 
 # 2. Remove unnecessary columns before further cleaning
-students_clean = students.drop(columns = ['COUNTRY', 'INDICATOR', 'Indicator',
-                                          'EDUCATION_LEV', 'Education level', 'SEX',
-                                          'MOBILITY', 'Mobility status','EDUCATION_FIELD', 
-                                          'YEAR', 'Flag Codes', 'Flags'])
+students_clean = students.drop(
+    columns=[
+        "COUNTRY",
+        "INDICATOR",
+        "Indicator",
+        "EDUCATION_LEV",
+        "Education level",
+        "SEX",
+        "MOBILITY",
+        "Mobility status",
+        "EDUCATION_FIELD",
+        "YEAR",
+        "Flag Codes",
+        "Flags",
+    ]
+)
 print(students_clean)
 
 
 # 3. Fix incorrect values
-students_clean = students_clean[students_clean['Year'] != "Latest available year"] 
+students_clean = students_clean[students_clean["Year"] != "Latest available year"]
 
 # 4. Count NAs across the whole dataframe
-students_clean.isna().sum() # 4,585 NAs in column "Value" --> these are useless to us
+students_clean.isna().sum()  # 4,585 NAs in column "Value" --> these are useless to us
 
 # Remove rows with NAs for "Value"
-students_clean = students_clean[students_clean['Value'].notna()]
+students_clean = students_clean[students_clean["Value"].notna()]
 
 # Filter out EU and OECD totals later
 
-# 5. Spread 
-students_wide = students_clean.pivot(index=['Country', 'Year', 'Field'],columns='Gender',values='Value')
+# 5. Spread
+students_wide = students_clean.pivot(
+    index=["Country", "Year", "Field"], columns="Gender", values="Value"
+)
 
 # Make indices into columns
-students_wide = students_wide.reset_index(level=['Country', 'Year', 'Field'])
+students_wide = students_wide.reset_index(level=["Country", "Year", "Field"])
 
 # 5. Feature generation
 ## Add column "Ratio" for the ratio of male to female entrants in each field (by year and country)
-students_wide['Ratio'] = students_wide['Male']/students_wide['Female']
-students_wide['Ratio'] = students_wide['Ratio'].replace(['inf'],0)
+students_wide["Ratio"] = students_wide["Male"] / students_wide["Female"]
+students_wide["Ratio"] = students_wide["Ratio"].replace(["inf"], 0)
 
 ## Add column "STEM_Status" to indicate whether the field is a stem or non-stem field
 students_wide["STEM_Status"] = np.select(
     [
-        students_wide["Field"].isin(['Agriculture, forestry, fisheries and veterinary', 'Engineering, manufacturing and construction',
-                                     'Health and welfare', 'Information and Communication Technologies (ICTs)',
-                                     'Natural sciences, mathematics and statistics']),
-        students_wide["Field"].isin(['Arts and humanities', 'Business, administration and law', 'Education',
-                                     'Generic programmes and qualifications', 'Services', 'Social sciences, journalism and information']),
+        students_wide["Field"].isin(
+            [
+                "Agriculture, forestry, fisheries and veterinary",
+                "Engineering, manufacturing and construction",
+                "Health and welfare",
+                "Information and Communication Technologies (ICTs)",
+                "Natural sciences, mathematics and statistics",
+            ]
+        ),
+        students_wide["Field"].isin(
+            [
+                "Arts and humanities",
+                "Business, administration and law",
+                "Education",
+                "Generic programmes and qualifications",
+                "Services",
+                "Social sciences, journalism and information",
+            ]
+        ),
     ],
     ["STEM", "Non-STEM"],
     default=" ",
 )
 
 # Write to csv
-students_wide.to_csv('Clean_Datasets/OECD_Students.csv', index=False)
-
+students_wide.to_csv("Clean_Datasets/OECD_Students.csv", index=False)
